122e4dd9573e63319af919d2d69bf62593c93406
arieljordz <arieljordz@gmail.com>
2025-05-13 16:53:43 +0800

    fix spinner

--- client/src/context/SpinnerContext.jsx
+++ client/src/context/SpinnerContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useContext } from "react";
-import Spinner from "../components/common/Spinner";
+import Spinner from "../components/common/Spinner"; 
 
 const SpinnerContext = createContext();
 
